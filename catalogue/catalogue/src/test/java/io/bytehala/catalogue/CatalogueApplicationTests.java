d8bc6c61c4e67c26908d372f9ea3e09bdd4e309c
Lem Dulfo <lemuel.dulfo@gmail.com>
2020-05-13 15:18:08 -0600

    Genesis of catalogue springboot project

---
+++ catalogue/catalogue/src/test/java/io/bytehala/catalogue/CatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bytehala.catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
